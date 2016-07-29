181fab853bd056050e04e516e5916941d81f4760
Nikita Glukhov <n.gluhov@postgrespro.ru>
2016-07-29 13:18:26 +0300

    Remove references to JsonbIterator.isScalar outside of jsonb_utils.c

--- src/backend/utils/adt/jsonfuncs.c
+++ src/backend/utils/adt/jsonfuncs.c
@@ -5445,10 +5445,8 @@ transform_jsonb_string_values(Jsonb *jsonb, void *action_state,
 	JsonbIteratorToken type;
 	JsonbParseState *st = NULL;
 	text	   *out;
-	bool		is_scalar = false;
 
 	it = JsonbIteratorInit(&jsonb->root);
-	is_scalar = it->isScalar;
 
 	while ((type = JsonbIteratorNext(&it, &v, false)) != WJB_DONE)
 	{
@@ -5468,7 +5466,7 @@ transform_jsonb_string_values(Jsonb *jsonb, void *action_state,
 	}
 
 	if (res->type == jbvArray)
-		res->val.array.rawScalar = is_scalar;
+		res->val.array.rawScalar = JB_ROOT_IS_SCALAR(jsonb);
 
 	return JsonbValueToJsonb(res);
 }
