71595a63671acd6fc8954a1e59d8bf84ef8ecf36
Nikita Glukhov <n.gluhov@postgrespro.ru>
2016-08-08 18:09:13 +0400

    Use PG_RETURN_JSONB()/JsonbGetDatum() instead of PG_RETURN_JSONB()/PointerGetDatum()

--- src/backend/utils/adt/jsonfuncs.c
+++ src/backend/utils/adt/jsonfuncs.c
@@ -1856,7 +1856,7 @@ each_worker_jsonb(FunctionCallInfo fcinfo, const char *funcname, bool as_text)
 				/* Not in text mode, just return the Jsonb */
 				Jsonb	   *val = JsonbValueToJsonb(&v);
 
-				values[1] = PointerGetDatum(val);
+				values[1] = JsonbPGetDatum(val);
 			}
 
 			tuple = heap_form_tuple(ret_tdesc, values, nulls);
@@ -2141,7 +2141,7 @@ elements_worker_jsonb(FunctionCallInfo fcinfo, const char *funcname,
 				/* Not in text mode, just return the Jsonb */
 				Jsonb	   *val = JsonbValueToJsonb(&v);
 
-				values[0] = PointerGetDatum(val);
+				values[0] = JsonbPGetDatum(val);
 			}
 
 			tuple = heap_form_tuple(ret_tdesc, values, nulls);
@@ -4111,7 +4111,7 @@ jsonb_strip_nulls(PG_FUNCTION_ARGS)
 	bool		last_was_key = false;
 
 	if (JB_ROOT_IS_SCALAR(jb))
-		PG_RETURN_POINTER(jb);
+		PG_RETURN_JSONB_P(jb);
 
 	it = JsonbIteratorInit(&jb->root);
 
@@ -4148,7 +4148,7 @@ jsonb_strip_nulls(PG_FUNCTION_ARGS)
 
 	Assert(res != NULL);
 
-	PG_RETURN_POINTER(JsonbValueToJsonb(res));
+	PG_RETURN_JSONB_P(JsonbValueToJsonb(res));
 }
 
 /*
@@ -4211,7 +4211,7 @@ jsonb_pretty(PG_FUNCTION_ARGS)
 Datum
 jsonb_canonical(PG_FUNCTION_ARGS)
 {
-	Jsonb	   *jb = PG_GETARG_JSONB(0);
+	Jsonb	   *jb = PG_GETARG_JSONB_P(0);
 	StringInfo	str = makeStringInfo();
 
 	JsonbToCStringCanonical(str, &jb->root, VARSIZE(jb));
