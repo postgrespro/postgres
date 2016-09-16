21e4f10a67410af74e393cd3f041ab7ec2cba6ee
Nikita Glukhov <n.gluhov@postgrespro.ru>
2016-09-17 00:17:43 +0400

    Add JsonValueUnwrap(), JsonExtractScalar()

--- src/backend/utils/adt/jsonb.c
+++ src/backend/utils/adt/jsonb.c
@@ -856,15 +856,10 @@ datum_to_jsonb(Datum val, bool is_null, JsonbInState *result,
 			case JSONBTYPE_JSONB:
 				{
 					Jsonb	   *jsonb = DatumGetJsonbP(val);
-					JsonbIterator *it;
-
-					it = JsonbIteratorInit(&jsonb->root);
 
 					if (JB_ROOT_IS_SCALAR(jsonb))
 					{
-						(void) JsonbIteratorNext(&it, &jb, true);
-						Assert(jb.type == jbvArray);
-						(void) JsonbIteratorNext(&it, &jb, true);
+						JsonExtractScalar(&jsonb->root, &jb);
 						break;
 					}
 
@@ -876,6 +871,7 @@ datum_to_jsonb(Datum val, bool is_null, JsonbInState *result,
 					else
 					{
 						JsonbIteratorToken type;
+						JsonbIterator *it = JsonbIteratorInit(&jsonb->root);
 
 						while ((type = JsonbIteratorNext(&it, &jb, false))
 							   != WJB_DONE)
