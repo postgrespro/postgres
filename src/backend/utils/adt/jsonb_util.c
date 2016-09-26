35131a79e0d211d012a005616eb3272eb0c5c263
Nikita Glukhov <n.gluhov@postgrespro.ru>
2016-09-26 17:28:10 +0400

    Remove JsonValue.val.binary.len field

--- src/backend/utils/adt/jsonb_util.c
+++ src/backend/utils/adt/jsonb_util.c
@@ -135,7 +135,7 @@ JsonContainerFlatten(JsonContainer *jc, JsonValueEncoder encoder,
 	{
 		jbv.type = jbvBinary;
 		jbv.val.binary.data = jc;
-		jbv.val.binary.len = jc->len;
+
 		binary = &jbv;
 	}
 
@@ -642,7 +642,6 @@ fillJsonbValue(const JsonbContainer *container, int index,
 						   (JsonbContainer *)(base_addr + INTALIGN(offset)),
 						   getJsonbLength(container, index) -
 								(INTALIGN(offset) - offset));
-		result->val.binary.len = result->val.binary.data->len;
 	}
 }
 
