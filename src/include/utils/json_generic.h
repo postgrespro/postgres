92847dd5b12560f7c14ddff312a2e3462768bccc
Nikita Glukhov <n.gluhov@postgrespro.ru>
2016-11-12 16:49:20 +0300

    Add JsonValueInitBinary()

--- src/include/utils/json_generic.h
+++ src/include/utils/json_generic.h
@@ -231,6 +231,15 @@ JsonIteratorFree(JsonIterator *it)
 		it = JsonIteratorFreeAndGetParent(it);
 }
 
+static inline JsonValue *
+JsonValueInitBinary(JsonValue *val, JsonContainer *cont)
+{
+	val->type = jbvBinary;
+	val->val.binary.data = cont;
+
+	return val;
+}
+
 extern Json *JsonValueToJson(JsonValue *val);
 extern JsonValue *JsonToJsonValue(Json *json, JsonValue *jv);
 extern JsonValue *JsonValueUnpackBinary(const JsonValue *jbv);
