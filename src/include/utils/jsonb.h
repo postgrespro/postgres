1c87221794b8cb3ad07356a7b60b21690adeb5ba
Nikita Glukhov <n.gluhov@postgrespro.ru>
2017-04-14 22:36:06 +0300

    Fix appendToBuffer() signature

--- src/include/utils/jsonb.h
+++ src/include/utils/jsonb.h
@@ -400,6 +400,6 @@ extern void JsonbHashScalarValueExtended(const JsonbValue *scalarVal,
 										 uint64 *hash, uint64 seed);
 
 extern int reserveFromBuffer(StringInfo buffer, int len);
-extern void appendToBuffer(StringInfo buffer, const char *data, int len);
+extern void appendToBuffer(StringInfo buffer, const void *data, int len);
 
 #endif							/* __JSONB_H__ */
