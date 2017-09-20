7ecf5e13cbee8a193d33864fba621743bf7412a5
Aleksandr Parfenov <a.parfenov@postgrespro.ru>
2017-09-20 13:58:15 +0300

    Update comments for TSConfigMap related fuctions

--- src/include/tsearch/ts_configmap.h
+++ src/include/tsearch/ts_configmap.h
@@ -20,14 +20,29 @@
  * Provide interface to convert ts_configuration into JSONB and vice versa
  */
 
+/* Convert TSMapRuleList structure into JSONB */
 extern Jsonb *TSMapToJsonb(TSMapRuleList *rules);
-extern TSMapRuleList *JsonbToTSMap(Jsonb *json);
+
+/* Extract TSMapRuleList from JSONB formated data */
+extern TSMapRuleList * JsonbToTSMap(Jsonb *json);
+/* Replace all occurances of oldDict by newDict */
 extern void TSMapReplaceDictionary(TSMapRuleList *rules, Oid oldDict, Oid newDict);
+
+/* Return list of all dictionries in rule list in order they are defined in the lsit as array of Oids */
 extern Oid *TSMapGetDictionariesList(TSMapRuleList *rules);
+
+/* Return list of all dictionries in rule list in order they are defined in the list as ListDictionary structure */
 extern ListDictionary *TSMapGetListDictionary(TSMapRuleList *rules);
-extern TSMapRuleList *TSMapMoveToMemoryContext(TSMapRuleList *rules, MemoryContext context);
+
+/* Move rule list into specified memory context */
+extern TSMapRuleList * TSMapMoveToMemoryContext(TSMapRuleList *rules, MemoryContext context);
+/* Free all nodes of the rule list */
 extern void TSMapFree(TSMapRuleList *rules);
+
+/* Print rule in human-readable format */
 extern void TSMapPrintRule(TSMapRule *rule, StringInfo result, int depth);
+
+/* Print rule list in human-readable format */
 extern void TSMapPrintRuleList(TSMapRuleList *rules, StringInfo result, int depth);
 
 #endif							/* _PG_TS_CONFIGMAP_H_ */
