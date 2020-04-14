9b8218c23637635ad560185524f5aee5738a24de
Nikita Glukhov <n.gluhov@postgrespro.ru>
2020-04-14 17:59:06 +0300

    Avoid double compilation of jsonb_op.c

--- src/backend/utils/adt/jsonb_op.c
+++ src/backend/utils/adt/jsonb_op.c
@@ -19,11 +19,9 @@
 #include "utils/jsonb.h"
 #include "utils/json_generic.h"
 
-Datum
-jsonb_exists(PG_FUNCTION_ARGS)
+static bool
+json_exists_internal(Json *jb, text *key)
 {
-	Jsonb	   *jb = PG_GETARG_JSONB_P(0);
-	text	   *key = PG_GETARG_TEXT_PP(1);
 	JsonbValue	kval;
 	JsonbValue *v = NULL;
 
@@ -41,14 +39,26 @@ jsonb_exists(PG_FUNCTION_ARGS)
 									JB_FOBJECT | JB_FARRAY,
 									&kval);
 
-	PG_RETURN_BOOL(v != NULL);
+	return v != NULL;
 }
 
 Datum
-jsonb_exists_any(PG_FUNCTION_ARGS)
+jsonb_exists(PG_FUNCTION_ARGS)
 {
-	Jsonb	   *jb = PG_GETARG_JSONB_P(0);
-	ArrayType  *keys = PG_GETARG_ARRAYTYPE_P(1);
+	PG_RETURN_BOOL(json_exists_internal(PG_GETARG_JSONB_P(0),
+										PG_GETARG_TEXT_PP(1)));
+}
+
+Datum
+json_exists(PG_FUNCTION_ARGS)
+{
+	PG_RETURN_BOOL(json_exists_internal(PG_GETARG_JSONT_P(0),
+										PG_GETARG_TEXT_PP(1)));
+}
+
+static bool
+json_exists_any_internal(Jsonb *jb, ArrayType *keys)
+{
 	int			i;
 	Datum	   *key_datums;
 	bool	   *key_nulls;
@@ -71,17 +81,29 @@ jsonb_exists_any(PG_FUNCTION_ARGS)
 		if (findJsonbValueFromContainer(&jb->root,
 										JB_FOBJECT | JB_FARRAY,
 										&strVal) != NULL)
-			PG_RETURN_BOOL(true);
+			return true;
 	}
 
-	PG_RETURN_BOOL(false);
+	return false;
 }
 
 Datum
-jsonb_exists_all(PG_FUNCTION_ARGS)
+jsonb_exists_any(PG_FUNCTION_ARGS)
 {
-	Jsonb	   *jb = PG_GETARG_JSONB_P(0);
-	ArrayType  *keys = PG_GETARG_ARRAYTYPE_P(1);
+	PG_RETURN_BOOL(json_exists_any_internal(PG_GETARG_JSONB_P(0),
+											PG_GETARG_ARRAYTYPE_P(1)));
+}
+
+Datum
+json_exists_any(PG_FUNCTION_ARGS)
+{
+	PG_RETURN_BOOL(json_exists_any_internal(PG_GETARG_JSONT_P(0),
+											PG_GETARG_ARRAYTYPE_P(1)));
+}
+
+static bool
+json_exists_all_internal(Jsonb *jb, ArrayType *keys)
+{
 	int			i;
 	Datum	   *key_datums;
 	bool	   *key_nulls;
@@ -104,126 +126,66 @@ jsonb_exists_all(PG_FUNCTION_ARGS)
 		if (findJsonbValueFromContainer(&jb->root,
 										JB_FOBJECT | JB_FARRAY,
 										&strVal) == NULL)
-			PG_RETURN_BOOL(false);
+			return false;
 	}
 
-	PG_RETURN_BOOL(true);
+	return true;
+}
+
+Datum
+jsonb_exists_all(PG_FUNCTION_ARGS)
+{
+	PG_RETURN_BOOL(json_exists_all_internal(PG_GETARG_JSONB_P(0),
+											PG_GETARG_ARRAYTYPE_P(1)));
+}
+
+Datum
+json_exists_all(PG_FUNCTION_ARGS)
+{
+	PG_RETURN_BOOL(json_exists_all_internal(PG_GETARG_JSONT_P(0),
+											PG_GETARG_ARRAYTYPE_P(1)));
+}
+
+static bool
+json_contains_internal(Json *val, Json *tmpl)
+{
+	if (JB_ROOT_IS_OBJECT(val) != JB_ROOT_IS_OBJECT(tmpl))
+		return false;
+
+	return JsonbDeepContains(JsonRoot(val), JsonRoot(tmpl));
 }
 
 Datum
 jsonb_contains(PG_FUNCTION_ARGS)
 {
-	Jsonb	   *val = PG_GETARG_JSONB_P(0);
-	Jsonb	   *tmpl = PG_GETARG_JSONB_P(1);
-
-	if (JB_ROOT_IS_OBJECT(val) != JB_ROOT_IS_OBJECT(tmpl))
-		PG_RETURN_BOOL(false);
-
-	PG_RETURN_BOOL(JsonbDeepContains(JsonRoot(val), JsonRoot(tmpl)));
+	PG_RETURN_BOOL(json_contains_internal(PG_GETARG_JSONB_P(0),
+										  PG_GETARG_JSONB_P(1)));
 }
 
+Datum
+json_contains(PG_FUNCTION_ARGS)
+{
+	PG_RETURN_BOOL(json_contains_internal(PG_GETARG_JSONT_P(0),
+										  PG_GETARG_JSONT_P(1)));
+}
+
+/* Commutator of "contains" */
 Datum
 jsonb_contained(PG_FUNCTION_ARGS)
 {
-	/* Commutator of "contains" */
-	Jsonb	   *tmpl = PG_GETARG_JSONB_P(0);
-	Jsonb	   *val = PG_GETARG_JSONB_P(1);
-
-	if (JB_ROOT_IS_OBJECT(val) != JB_ROOT_IS_OBJECT(tmpl))
-		PG_RETURN_BOOL(false);
-
-	PG_RETURN_BOOL(JsonbDeepContains(JsonRoot(val), JsonRoot(tmpl)));
+	PG_RETURN_BOOL(json_contains_internal(PG_GETARG_JSONB_P(1),
+										  PG_GETARG_JSONB_P(0)));
 }
 
 Datum
-jsonb_ne(PG_FUNCTION_ARGS)
+json_contained(PG_FUNCTION_ARGS)
 {
-	Jsonb	   *jba = PG_GETARG_JSONB_P(0);
-	Jsonb	   *jbb = PG_GETARG_JSONB_P(1);
-	bool		res;
-
-	res = (compareJsonbContainers(&jba->root, &jbb->root) != 0);
-
-	PG_FREE_IF_COPY_JSONB(jba, 0);
-	PG_FREE_IF_COPY_JSONB(jbb, 1);
-	PG_RETURN_BOOL(res);
+	PG_RETURN_BOOL(json_contains_internal(PG_GETARG_JSONT_P(1),
+										  PG_GETARG_JSONT_P(0)));
 }
 
-/*
- * B-Tree operator class operators, support function
- */
-Datum
-jsonb_lt(PG_FUNCTION_ARGS)
-{
-	Jsonb	   *jba = PG_GETARG_JSONB_P(0);
-	Jsonb	   *jbb = PG_GETARG_JSONB_P(1);
-	bool		res;
-
-	res = (compareJsonbContainers(&jba->root, &jbb->root) < 0);
-
-	PG_FREE_IF_COPY_JSONB(jba, 0);
-	PG_FREE_IF_COPY_JSONB(jbb, 1);
-	PG_RETURN_BOOL(res);
-}
-
-Datum
-jsonb_gt(PG_FUNCTION_ARGS)
-{
-	Jsonb	   *jba = PG_GETARG_JSONB_P(0);
-	Jsonb	   *jbb = PG_GETARG_JSONB_P(1);
-	bool		res;
-
-	res = (compareJsonbContainers(&jba->root, &jbb->root) > 0);
-
-	PG_FREE_IF_COPY_JSONB(jba, 0);
-	PG_FREE_IF_COPY_JSONB(jbb, 1);
-	PG_RETURN_BOOL(res);
-}
-
-Datum
-jsonb_le(PG_FUNCTION_ARGS)
-{
-	Jsonb	   *jba = PG_GETARG_JSONB_P(0);
-	Jsonb	   *jbb = PG_GETARG_JSONB_P(1);
-	bool		res;
-
-	res = (compareJsonbContainers(&jba->root, &jbb->root) <= 0);
-
-	PG_FREE_IF_COPY_JSONB(jba, 0);
-	PG_FREE_IF_COPY_JSONB(jbb, 1);
-	PG_RETURN_BOOL(res);
-}
-
-Datum
-jsonb_ge(PG_FUNCTION_ARGS)
-{
-	Jsonb	   *jba = PG_GETARG_JSONB_P(0);
-	Jsonb	   *jbb = PG_GETARG_JSONB_P(1);
-	bool		res;
-
-	res = (compareJsonbContainers(&jba->root, &jbb->root) >= 0);
-
-	PG_FREE_IF_COPY_JSONB(jba, 0);
-	PG_FREE_IF_COPY_JSONB(jbb, 1);
-	PG_RETURN_BOOL(res);
-}
-
-Datum
-jsonb_eq(PG_FUNCTION_ARGS)
-{
-	Jsonb	   *jba = PG_GETARG_JSONB_P(0);
-	Jsonb	   *jbb = PG_GETARG_JSONB_P(1);
-	bool		res;
-
-	res = (compareJsonbContainers(&jba->root, &jbb->root) == 0);
-
-	PG_FREE_IF_COPY_JSONB(jba, 0);
-	PG_FREE_IF_COPY_JSONB(jbb, 1);
-	PG_RETURN_BOOL(res);
-}
-
-Datum
-jsonb_cmp(PG_FUNCTION_ARGS)
+static int
+jsonb_cmp_internal(FunctionCallInfo fcinfo)
 {
 	Jsonb	   *jba = PG_GETARG_JSONB_P(0);
 	Jsonb	   *jbb = PG_GETARG_JSONB_P(1);
@@ -233,23 +195,126 @@ jsonb_cmp(PG_FUNCTION_ARGS)
 
 	PG_FREE_IF_COPY_JSONB(jba, 0);
 	PG_FREE_IF_COPY_JSONB(jbb, 1);
-	PG_RETURN_INT32(res);
+
+	return res;
+}
+
+static int
+json_cmp_internal(FunctionCallInfo fcinfo)
+{
+	Json	   *jba = PG_GETARG_JSONT_P(0);
+	Json	   *jbb = PG_GETARG_JSONT_P(1);
+	int			res;
+
+	res = compareJsonbContainers(&jba->root, &jbb->root);
+
+	PG_FREE_IF_COPY_JSONB(jba, 0);
+	PG_FREE_IF_COPY_JSONB(jbb, 1);
+
+	return res;
+}
+
+Datum
+jsonb_ne(PG_FUNCTION_ARGS)
+{
+	PG_RETURN_BOOL(jsonb_cmp_internal(fcinfo) != 0);
+}
+
+Datum
+json_ne(PG_FUNCTION_ARGS)
+{
+	PG_RETURN_BOOL(json_cmp_internal(fcinfo) != 0);
 }
 
 /*
- * Hash operator class jsonb hashing function
+ * B-Tree operator class operators, support function
  */
 Datum
-jsonb_hash(PG_FUNCTION_ARGS)
+jsonb_lt(PG_FUNCTION_ARGS)
 {
-	Jsonb	   *jb = PG_GETARG_JSONB_P(0);
+	PG_RETURN_BOOL(jsonb_cmp_internal(fcinfo) < 0);
+}
+
+Datum
+json_lt(PG_FUNCTION_ARGS)
+{
+	PG_RETURN_BOOL(json_cmp_internal(fcinfo) < 0);
+}
+
+Datum
+jsonb_gt(PG_FUNCTION_ARGS)
+{
+	PG_RETURN_BOOL(jsonb_cmp_internal(fcinfo) > 0);
+}
+
+Datum
+json_gt(PG_FUNCTION_ARGS)
+{
+	PG_RETURN_BOOL(json_cmp_internal(fcinfo) > 0);
+}
+
+Datum
+jsonb_le(PG_FUNCTION_ARGS)
+{
+	PG_RETURN_BOOL(jsonb_cmp_internal(fcinfo) <= 0);
+}
+
+Datum
+json_le(PG_FUNCTION_ARGS)
+{
+	PG_RETURN_BOOL(json_cmp_internal(fcinfo) <= 0);
+}
+
+Datum
+jsonb_ge(PG_FUNCTION_ARGS)
+{
+	PG_RETURN_BOOL(jsonb_cmp_internal(fcinfo) >= 0);
+}
+
+Datum
+json_ge(PG_FUNCTION_ARGS)
+{
+	PG_RETURN_BOOL(json_cmp_internal(fcinfo) >= 0);
+}
+
+Datum
+jsonb_eq(PG_FUNCTION_ARGS)
+{
+	PG_RETURN_BOOL(jsonb_cmp_internal(fcinfo) == 0);
+}
+
+Datum
+json_eq(PG_FUNCTION_ARGS)
+{
+	PG_RETURN_BOOL(json_cmp_internal(fcinfo) == 0);
+}
+
+Datum
+jsonb_cmp(PG_FUNCTION_ARGS)
+{
+	PG_RETURN_INT32(jsonb_cmp_internal(fcinfo));
+}
+
+Datum
+json_cmp(PG_FUNCTION_ARGS)
+{
+	PG_RETURN_INT32(json_cmp_internal(fcinfo));
+}
+
+static Datum
+json_hash_internal(FunctionCallInfo fcinfo, bool is_jsonb)
+{
+	Json	   *jb = is_jsonb ? PG_GETARG_JSONB_P(0) : PG_GETARG_JSONT_P(0);
 	JsonbIterator *it;
 	JsonbValue	v;
 	JsonbIteratorToken r;
 	uint32		hash = 0;
 
 	if (JB_ROOT_COUNT(jb) == 0)
+	{
+		PG_FREE_IF_COPY_JSONB(jb, 0);
 		PG_RETURN_INT32(0);
+	}
 
 	it = JsonbIteratorInit(&jb->root);
 
@@ -281,10 +346,25 @@ jsonb_hash(PG_FUNCTION_ARGS)
 	PG_RETURN_INT32(hash);
 }
 
+/*
+ * Hash operator class jsonb hashing function
+ */
 Datum
-jsonb_hash_extended(PG_FUNCTION_ARGS)
+jsonb_hash(PG_FUNCTION_ARGS)
 {
-	Jsonb	   *jb = PG_GETARG_JSONB_P(0);
+	return json_hash_internal(fcinfo, true);
+}
+
+Datum
+json_hash(PG_FUNCTION_ARGS)
+{
+	return json_hash_internal(fcinfo, false);
+}
+
+static Datum
+json_hash_extended_internal(FunctionCallInfo fcinfo, bool is_jsonb)
+{
+	Json	   *jb = is_jsonb ? PG_GETARG_JSONB_P(0) : PG_GETARG_JSONT_P(0);
 	uint64		seed = PG_GETARG_INT64(1);
 	JsonbIterator *it;
 	JsonbValue	v;
@@ -322,4 +402,16 @@ jsonb_hash_extended(PG_FUNCTION_ARGS)
 
 	PG_FREE_IF_COPY_JSONB(jb, 0);
 	PG_RETURN_UINT64(hash);
+}
+
+Datum
+jsonb_hash_extended(PG_FUNCTION_ARGS)
+{
+	return json_hash_extended_internal(fcinfo, true);
+}
+
+Datum
+json_hash_extended(PG_FUNCTION_ARGS)
+{
+	return json_hash_extended_internal(fcinfo, false);
 }
