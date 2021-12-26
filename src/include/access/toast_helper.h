e4371a296bd0b1a815db162d970dc22d54e39c8e
Nikita Malakhov <n.malakhov@postgrespro.ru>
2021-12-27 00:39:17 +0300

    Double header declarations fixed

--- src/include/access/toast_helper.h
+++ src/include/access/toast_helper.h
@@ -126,10 +126,38 @@ extern void toast_delete_external(Relation rel, Datum *values, bool *isnull,
 extern Datum toast_compress_datum(Datum value, char cmethod);
 extern struct varlena *toast_decompress_datum(struct varlena *attr);
 extern struct varlena *toast_decompress_datum_slice(struct varlena *attr, int32 slicelength);
-extern struct varlena *detoast_attr_slice(struct varlena *attr,
-				   int32 sliceoffset, int32 slicelength);
-extern struct varlena *detoast_attr(struct varlena *attr);
+
+/* ----------
+ * detoast_external_attr() -
+ *
+ *		Fetches an external stored attribute from the toast
+ *		relation. Does NOT decompress it, if stored external
+ *		in compressed format.
+ * ----------
+ */
 extern struct varlena *detoast_external_attr(struct varlena *attr);
+
+/* ----------
+ * detoast_attr() -
+ *
+ *		Fully detoasts one attribute, fetching and/or decompressing
+ *		it as needed.
+ * ----------
+ */
+extern struct varlena *detoast_attr(struct varlena *attr);
+
+/* ----------
+ * detoast_attr_slice() -
+ *
+ *		Fetches only the specified portion of an attribute.
+ *		(Handles all cases for attribute storage)
+ * ----------
+ */
+extern struct varlena *detoast_attr_slice(struct varlena *attr,
+										  int32 sliceoffset,
+										  int32 slicelength);
+
+
 /* ----------
  * toast_raw_datum_size -
  *
