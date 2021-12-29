227a2fa284f4c14ecfc70abbf7e050a5f3b22012
Nikita Malakhov <n.malakhov@postgrespro.ru>
2021-12-29 15:58:27 +0300

    Dummy_toaster tests

--- contrib/dummy_toaster/sql/dummy_toaster.sql
+++ contrib/dummy_toaster/sql/dummy_toaster.sql
@@ -1,5 +1,4 @@
 CREATE EXTENSION dummy_toaster;
-
 CREATE TABLE tst_failed (
 	t text TOASTER dummy_toaster TOASTER dummy_toaster
 );
@@ -9,7 +8,6 @@ CREATE TABLE tst1 (
 	t text STORAGE external TOASTER dummy_toaster,
 	l int
 );
-
 SELECT  setseed(0);
 
 INSERT INTO tst1
@@ -18,7 +16,6 @@ INSERT INTO tst1
 			repeat(random()::text, (20+30*random())::int) as t
 		 FROM
 			generate_series(1, 32) as  i) as t;
-
 SELECT length(t), l, length(t) = l FROM tst1 ORDER BY 1, 3;
 
 SELECT attnum, attname, atttypid, attstorage, tsrname 
@@ -27,7 +24,7 @@ SELECT attnum, attname, atttypid, attstorage, tsrname
 	ORDER BY attnum;
 
 CREATE TABLE tst2 (
-	t	text
+	t text
 );
 
 SELECT attnum, attname, atttypid, attstorage, tsrname 
@@ -36,11 +33,48 @@ SELECT attnum, attname, atttypid, attstorage, tsrname
 	ORDER BY attnum;
 
 ALTER TABLE tst2 ALTER COLUMN t SET TOASTER dummy_toaster;
-
 SELECT attnum, attname, atttypid, attstorage, tsrname 
 	FROM pg_attribute, pg_toaster t
 	WHERE attrelid = 'tst2'::regclass and attnum>0 and t.oid = atttoaster
 	ORDER BY attnum;
 
-\d+ tst1
-\d+ tst2
+CREATE TABLE tst3 (
+	f text STORAGE external TOASTER deftoaster,
+	t text STORAGE external TOASTER dummy_toaster,
+	l int
+);
+
+alter table tst3 add id serial;
+
+INSERT INTO tst3
+	SELECT repeat('a', 2000)::text as f, t.t as t, length(t.t) as l FROM
+		(SELECT
+			repeat(random()::text, (20+30*random())::int) as t
+		 FROM
+			generate_series(1, 32) as  i) as t;
+SELECT length(t), l, length(t) = l FROM tst1 ORDER BY 1, 3;
+
+SELECT attnum, attname, atttypid, attstorage, tsrname 
+	FROM pg_attribute, pg_toaster t
+	WHERE attrelid = 'tst3'::regclass and attnum>0 and t.oid = atttoaster
+	ORDER BY attnum;
+
+update tst3 set f = repeat('b', 2000)::text;
+
+update tst3 set t = repeat('c', 2000)::text;
+
+ALTER TABLE tst3 ALTER COLUMN f SET TOASTER dummy_toaster;
+
+update tst3 set f = repeat('d', 2000)::text;
+
+ALTER TABLE tst3 ALTER COLUMN t SET TOASTER deftoaster;
+
+update tst3 set t = repeat('e', 2000)::text;
+
+SELECT l, left(f,20), left(t,20) FROM tst3;
+
+SELECT attnum, attname, atttypid, attstorage, tsrname 
+	FROM pg_attribute, pg_toaster t
+	WHERE attrelid = 'tst3'::regclass and attnum>0 and t.oid = atttoaster
+	ORDER BY attnum;
+
