a326cc7269eec56dea29edc6f1b443bc101585b7
Nikita Glukhov <n.gluhov@postgrespro.ru>
2022-01-21 22:46:02 +0300

    fixup! Rename functions with two "Stats" in name, add two helper macros for jsonStatsGetPathByStr()

--- src/include/utils/json_selfuncs.h
+++ src/include/utils/json_selfuncs.h
@@ -70,8 +70,8 @@ typedef enum JsonStatType
 extern bool jsonStatsInit(JsonStats stats, const VariableStatData *vardata);
 extern void jsonStatsRelease(JsonStats data);
 
-extern JsonPathStats jsonStatsGetPathStatsStr(JsonStats stats,
-												const char *path, int pathlen);
+extern JsonPathStats jsonStatsGetPathByStr(JsonStats stats,
+										   const char *path, int pathlen);
 
 extern JsonPathStats jsonPathStatsGetSubpath(JsonPathStats stats,
 											 const char *subpath);
